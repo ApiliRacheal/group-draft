ce053dd768b4c5795b94db003de51c420874abd5
ApiliRacheal <133926447+ApiliRacheal@users.noreply.github.com>
2023-05-18 13:04:49 +1000

    Add files via upload

---
+++ w11.js
@@ -0,0 +1,24 @@
+
+
+
+
+const svg = document.getElementById("rect-svg");
+
+window.addEventListener("resize", resizeSvg);
+
+function resizeSvg(){
+    let bbox = svg.getBoundingClientRect();
+    svg.setAttribute("viewBox", `0 0 ${bbox.width} ${bbox.height}`);
+}
+
+
+function resizeSvg(){
+    let bbox = svg.getBoundingClientRect();
+    svg.setAttribute("viewBox", `0 0 ${bbox.width} ${bbox.height}`);
+    
+    for(let circle of svg.children){
+        circle.setAttribute('r',  Math.min(bbox.width, bbox.height) * 0.1);
+    }
+}
+
+
